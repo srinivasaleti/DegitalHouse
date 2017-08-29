48d251b4476d5933594c2092244702f40a123232
srinivasarao <srinivra@thoughtworks.com>
2017-08-29 16:18:08 +0530

    Work with light of a digitalHouse

--- src/com/tw/DigitalHouseApp.java
+++ src/com/tw/DigitalHouseApp.java
@@ -5,13 +5,29 @@ class DigitalHouseApp {
 
     public static void main(String[] args) {
         IO consoleIO = new ConsoleIO(System.out);
+        workWithFan(consoleIO);
+        workWithLight(consoleIO);
+    }
+
+    private static void workWithFan(IO consoleIO) {
+        consoleIO.println("Fan::");
         ElectricalDevice fan = new Fan(consoleIO);
         Switch aSwitch = new Switch(fan);
 
         aSwitch.press();
         aSwitch.press();
+        consoleIO.println("");
+    }
+
+    private static void workWithLight(IO consoleIO) {
+        consoleIO.println("Light::");
+        ElectricalDevice light = new Light(consoleIO);
+        Switch aSwitch = new Switch(light);
+
         aSwitch.press();
         aSwitch.press();
+        aSwitch.press();
+        consoleIO.println("");
     }
 
 }
